fe04ff8e9924b7493f3ca99d04096eb137f61185
Sakunthala-1-12 <sakunthaladavid97@gmail.com>
2026-07-09 10:11:28 +0530

    Create 110-BalancedBinaryTree

---
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.py
@@ -0,0 +1,16 @@
+# Last updated: 7/9/2026, 10:11:29 AM
+class Solution:
+    def isBalanced(self, root):
+        def height(node):
+            if not node:
+                return 0
+
+            left = height(node.left)
+            right = height(node.right)
+
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+
+            return 1 + max(left, right)
+
+        return height(root) != -1
