fbf9a79e58e95ac159528298a59f4c873d68cc85
Sakunthala-1-12 <sakunthaladavid97@gmail.com>
2026-07-09 10:10:28 +0530

    Create 257-BinaryTreePaths

---
+++ 257-BinaryTreePaths/257-BinaryTreePaths.py
@@ -0,0 +1,22 @@
+# Last updated: 7/9/2026, 10:10:29 AM
+class Solution:
+    def binaryTreePaths(self, root):
+        result = []
+
+        def dfs(node, path):
+            if not node:
+                return
+
+            path += str(node.val)
+
+            if not node.left and not node.right:
+                result.append(path)
+                return
+
+            path += "->"
+
+            dfs(node.left, path)
+            dfs(node.right, path)
+
+        dfs(root, "")
+        return result
