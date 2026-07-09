0e42c85e1072a78a03c64078c0ff9693f1788b3c
Sakunthala-1-12 <sakunthaladavid97@gmail.com>
2026-07-09 10:12:51 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,13 @@
+# Last updated: 7/9/2026, 10:12:52 AM
+class Solution:
+    def twoSum(self, nums, target):
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+
+
+
+        
