f8a48fbef927055f134afd72439cfab9c4ba1313
Sakunthala-1-12 <sakunthaladavid97@gmail.com>
2026-07-27 15:14:58 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/version/python/67-AddBinary_20260727_151459.py
@@ -0,0 +1,17 @@
+# Last updated: 7/27/2026, 3:14:59 PM
+1class Solution(object):
+2    def combine(self, n, k):
+3        result = []
+4
+5        def backtrack(start, path):
+6            if len(path) == k:
+7                result.append(path[:])
+8                return
+9
+10            for i in range(start, n + 1):
+11                path.append(i)
+12                backtrack(i + 1, path)
+13                path.pop()
+14
+15        backtrack(1, [])
+16        return result
